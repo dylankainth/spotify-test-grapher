13dcfe243b8855a06a901638f9bafad53839faf4
Dylan K <kainth.dylan@gmail.com>
2022-08-16 15:36:21 +0100

    graph kind works

--- pages/playlists/_id.vue
+++ pages/playlists/_id.vue
@@ -30,6 +30,96 @@
 </template>
 
 <script>
+// export default {
+//   data() {
+//     return {
+//       songs: {},
+//       playlist: null,
+//       ctx: null,
+//       chart: null,
+//     }
+//   },
+//   methods: {
+//     getdata() {
+//       for (let playlist in this.$store.state.playlists) {
+//         if (this.$store.state.playlists[playlist].id == this.$route.params.id) {
+//           this.playlist = this.$store.state.playlists[playlist]
+//           break
+//         }
+//       }
+
+//       const sets = parseInt((this.playlist.tracks.total + 4) / 5)
+
+//       for (let i = 0; i < sets; i++) {
+//         this.$axios
+//           .$get(
+//             'https://api.spotify.com/v1/playlists/' +
+//               this.$route.params.id +
+//               '/tracks?fields=items(track(name%2Cid%2Cartists))&limit=5&offset=' +
+//               i * 5
+//           )
+//           .then((response) => {
+//             for (let k = 0; k < response.items.length; k++) {
+//               console.log(i, i * 5, k, i * 5 + k, response.items[k].track.name)
+
+//               this.songs[response.items[k].track.id] = response.items[k].track
+
+//               this.songs[response.items[k].track.id].number = i * 5 + k
+//             }
+
+//             this.$axios
+//               .$get(
+//                 'https://api.spotify.com/v1/audio-features?ids=' +
+//                   Object.keys(this.songs).join(',')
+//               )
+//               .then((response) => {
+//                 for (let j = 0; j < response.audio_features.length; j++) {
+//                   this.songs[response.audio_features[j].id].audiofeatures =
+//                     response.audio_features[j]
+//                 }
+//               })
+//           })
+//       }
+
+//     },
+//   },
+//   mounted() {
+
+//     this.getdata()
+
+//     var chartdata = Object.values(this.songs)
+//     console.log(chartdata)
+
+//     this.ctx = document.getElementById('myChart').getContext('2d')
+//     this.chart = new Chart(this.ctx, {
+//       type: 'scatter',
+//       data: {
+//         datasets: [
+//           {
+//             label: 'Scatter Dataset',
+//             data: chartdata,
+//             backgroundColor: 'rgb(256,256,256)',
+//           },
+//         ],
+//       },
+//       options: {
+//         parsing: {
+//           xAxisKey: 'number',
+//           yAxisKey: 'audiofeatures.energy',
+//         },
+//         scales: {
+//           x: {
+//             type: 'linear',
+//             position: 'bottom',
+//           },
+//         },
+//       },
+//     })
+//   },
+// }
+</script>
+
+<script>
 export default {
   data() {
     return {
@@ -37,83 +127,85 @@ export default {
       playlist: null,
       ctx: null,
       chart: null,
+      loaded: null,
     }
   },
-  methods: {
-    getdata() {
-      for (let playlist in this.$store.state.playlists) {
-        if (this.$store.state.playlists[playlist].id == this.$route.params.id) {
-          this.playlist = this.$store.state.playlists[playlist]
-          break
-        }
-      }
+  watch: {
+    // whenever question changes, this function will run
+    loaded(newQuestion, oldQuestion) {
+      var chartdata = Object.values(this.songs)
+      console.log(chartdata)
 
-      const sets = parseInt((this.playlist.tracks.total + 4) / 5)
+      this.ctx = document.getElementById('myChart').getContext('2d')
+      this.chart = new Chart(this.ctx, {
+        type: 'scatter',
+        data: {
+          datasets: [
+            {
+              label: 'Scatter Dataset',
+              data: chartdata,
+              backgroundColor: 'rgb(256,256,256)',
+            },
+          ],
+        },
+        options: {
+          parsing: {
+            xAxisKey: 'number',
+            yAxisKey: 'audiofeatures.energy',
+          },
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+            },
+          },
+        },
+      })
 
-      for (let i = 0; i < sets; i++) {
-        this.$axios
-          .$get(
-            'https://api.spotify.com/v1/playlists/' +
-              this.$route.params.id +
-              '/tracks?fields=items(track(name%2Cid%2Cartists))&limit=5&offset=' +
-              i * 5
-          )
-          .then((response) => {
-            for (let k = 0; k < response.items.length; k++) {
-              console.log(i, i * 5, k, i * 5 + k, response.items[k].track.name)
-
-              this.songs[response.items[k].track.id] = response.items[k].track
-
-              this.songs[response.items[k].track.id].number = i * 5 + k
-            }
-
-            this.$axios
-              .$get(
-                'https://api.spotify.com/v1/audio-features?ids=' +
-                  Object.keys(this.songs).join(',')
-              )
-              .then((response) => {
-                for (let j = 0; j < response.audio_features.length; j++) {
-                  this.songs[response.audio_features[j].id].audiofeatures =
-                    response.audio_features[j]
-                }
-              })
-          })
-      }
+      this.chart.update()
     },
   },
   mounted() {
+    for (let playlist in this.$store.state.playlists) {
+      if (this.$store.state.playlists[playlist].id == this.$route.params.id) {
+        this.playlist = this.$store.state.playlists[playlist]
+        break
+      }
+    }
 
-    this.getdata()
+    const sets = parseInt((this.playlist.tracks.total + 4) / 5)
 
-    var chartdata = Object.values(this.songs)
-    console.log(chartdata)
+    for (let i = 0; i < sets; i++) {
+      this.$axios
+        .$get(
+          'https://api.spotify.com/v1/playlists/' +
+            this.$route.params.id +
+            '/tracks?fields=items(track(name%2Cid%2Cartists))&limit=5&offset=' +
+            i * 5
+        )
+        .then((response) => {
+          for (let k = 0; k < response.items.length; k++) {
+            console.log(i, i * 5, k, i * 5 + k, response.items[k].track.name)
 
-    this.ctx = document.getElementById('myChart').getContext('2d')
-    this.chart = new Chart(this.ctx, {
-      type: 'scatter',
-      data: {
-        datasets: [
-          {
-            label: 'Scatter Dataset',
-            data: chartdata,
-            backgroundColor: 'rgb(256,256,256)',
-          },
-        ],
-      },
-      options: {
-        parsing: {
-          xAxisKey: 'number',
-          yAxisKey: 'audiofeatures.energy',
-        },
-        scales: {
-          x: {
-            type: 'linear',
-            position: 'bottom',
-          },
-        },
-      },
-    })
+            this.songs[response.items[k].track.id] = response.items[k].track
+
+            this.songs[response.items[k].track.id].number = i * 5 + k
+          }
+
+          this.$axios
+            .$get(
+              'https://api.spotify.com/v1/audio-features?ids=' +
+                Object.keys(this.songs).join(',')
+            )
+            .then((response) => {
+              for (let j = 0; j < response.audio_features.length; j++) {
+                this.songs[response.audio_features[j].id].audiofeatures =
+                  response.audio_features[j]
+                this.loaded = true
+              }
+            })
+        })
+    }
   },
 }
 </script>
